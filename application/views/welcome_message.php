170880441dc7e61760d5f905f214d36181bd26a4
Mike <michaxze@gmail.com>
2011-04-22 05:22:44 +0800

    new code igniter files

---
+++ application/views/welcome_message.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tilbud i Byen</title>
+	<link href="css/main_style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+<div id="header">
+  <img src="images/ui/logo8.png">
+</div>
+<div class="clear"></div>
+
+<div id="content">
+  <p> content </p>
+</div>
+<div class="clear"></div>
+
+<div id="content2">
+  <p> content2</p>
+</div>
+<div class="clear"></div>
+
+<div id="footer">
+  <p> Footer </p>
+</div>
+
+</body>
+</html>
