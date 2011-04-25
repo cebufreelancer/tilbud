ff39fc16409a143e50671984258abeb74b02658d
Mike <michael@cebudirectories.com>
2011-04-25 10:15:42 +0800

    added users module

---
+++ kohana/modules/users/classes/controller/admin/user.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Admin_User extends Controller_Useradmin_Admin_User {}
---
+++ kohana/modules/users/init.php
@@ -0,0 +1,23 @@
+<?php
+Route::set('user/provider', 'user/provider(/<provider>)', array('provider' => '.+'))
+	->defaults(array(
+		'controller' => 'user',
+		'action'     => 'provider',
+		'provider'       => NULL,
+	));
+
+Route::set('user/provider_return', 'user/provider_return(/<provider>)', array('provider' => '.+'))
+	->defaults(array(
+		'controller' => 'user',
+		'action'     => 'provider_return',
+		'provider'       => NULL,
+	));
+
+// Static file serving (CSS, JS, images)
+Route::set('css', '<dir>(/<file>)', array('file' => '.+', 'dir' => '(css|img)'))
+   ->defaults(array(
+		'controller' => 'user',
+		'action'     => 'media',
+		'file'       => NULL,
+		'dir'       => NULL,
+	));
