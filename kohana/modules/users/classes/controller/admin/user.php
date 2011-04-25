c56a18bb14454c1c79054c6918c09d62758969bd
Mike <michael@cebudirectories.com>
2011-04-25 10:17:27 +0800

    move users to user

--- kohana/modules/users/classes/controller/admin/user.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Controller_Admin_User extends Controller_Useradmin_Admin_User {}
--- kohana/modules/users/config/facebook.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-return array(
-   'app_id'          => '',
-   'api_key'         => '',
-   'secret'          => '',
-);
