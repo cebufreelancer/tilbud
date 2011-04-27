2fa4a35896ef460cf1bfc7984b56cc187a57c336
Paul Villacorta <pwvillacorta@gmail.com>
2011-04-27 23:51:36 +0800

    added user model

---
+++ kohana/application/classes/model/user.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Model_User extends Model_Useradmin_User {
+
+	// This class can be replaced or extended
+	
+} // End User Model
