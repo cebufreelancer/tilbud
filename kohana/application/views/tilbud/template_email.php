08f79c2adebc68e09aeac5b9c2b261fe5157cb06
Paul Villacorta <pwvillacorta@gmail.com>
2011-05-06 08:31:09 +0800

    modified template

--- kohana/application/views/tilbud/template_email.php
+++ kohana/application/views/tilbud/template_email.php
@@ -1,45 +1,39 @@
-<html>
-<head>
-  <style type="text/css" media=screen>
-	<!--
-		body { font-family: "Arial", "Helvetica", sans-serif; }
-    a { color: #40a2d5; }
-		#main { border: 15px solid #40a2d5; background: #FFFFFF; width: 670px; padding: 15px; margin: auto; }
-		#main-title { background: #000000; padding: 15px 10px;
-									url(<?php echo url::base(TRUE)?>images/bg-header.png) bottom left repeat-x; }
-		#deal-title { background: #40a2d5; color: #FFF; }
-		#deal-title h1 { font-size: 22px; padding: 10px; }
-		#deal-control { width: 200px; float: left; }
-		#deal-content { width: 430px; float: left; padding-left: 25px; font: 18px/22px Arial, Helvetica, sans-serif; }
-		#deal-content h2 { color: #666666; font-size: 22px; }
-    #deal-content img { margin-bottom: 20px; }
-	
-		.price { font-size: 60px; font-weight: bold; text-align: center; color: #40a2d5; }
-    .medium { font-size: 45px; }
-		.button { background: url(<?php echo url::base(TRUE)?>images/bg-button.png) top left repeat-x; 
-							color: #FFFFFF; font-weight: bold; font-size: 28px; 
-							padding: 10px 15px; text-align: center; margin-bottom: 20px;
-							border-radius: 10px; display: block; text-decoration: none; 
-							-moz-border-radius: 10px;
-							-webkit-border-radius: 10px;
-							-moz-box-shadow: 0px 0px 20px #333 inset;
-							-webkit-box-shadow: 0px 0px 20px #333 inset;
-							box-shadow: 0px 0px 20px #333 inset;
-						}
-		.big { font-size: 18px; font-weight: bold; padding: 5px 20px 5px 0px; }
-		.align-right { float: right; }
-		.align-center { text-align: center; font-size: 11px; }
-		.subhead { color: #40a2d5; }
-		.contents { font-size: 14px; }
-		.slogan { float: right; font-style: italic; color: #e9e9e9; font-size: 17px; padding-top: 15px; }
-		.black { color: #000; }
-		.clear { clear: both; }
-    ::selection { background: #F85510; /* Safari */ }
-		::-moz-selection { background: #F85510; /* Firefox */ }
-	-->
-	</style>
-</head>
-<body>
+<style type="text/css">
+  body { font-family: "Arial", "Helvetica", sans-serif; }
+  a { color: #40a2d5; }
+  #main { border: 15px solid #40a2d5; background: #FFFFFF; width: 670px; padding: 15px; margin: auto; }
+  #main-title { background: #000000; padding: 15px 10px;
+                url(<?php echo url::base(TRUE)?>images/bg-header.png) bottom left repeat-x; }
+  #deal-title { background: #40a2d5; color: #FFF; }
+  #deal-title h1 { font-size: 22px; padding: 10px; }
+  #deal-control { width: 200px; float: left; }
+  #deal-content { width: 430px; float: left; padding-left: 25px; font: 18px/22px Arial, Helvetica, sans-serif; }
+  #deal-content h2 { color: #666666; font-size: 22px; }
+  #deal-content img { margin-bottom: 20px; }
+
+  .price { font-size: 60px; font-weight: bold; text-align: center; color: #40a2d5; }
+  .medium { font-size: 45px; }
+  .button { background: url(<?php echo url::base(TRUE)?>images/bg-button.png) top left repeat-x; 
+            color: #FFFFFF; font-weight: bold; font-size: 28px; 
+            padding: 10px 15px; text-align: center; margin-bottom: 20px;
+            border-radius: 10px; display: block; text-decoration: none; 
+            -moz-border-radius: 10px;
+            -webkit-border-radius: 10px;
+            -moz-box-shadow: 0px 0px 20px #333 inset;
+            -webkit-box-shadow: 0px 0px 20px #333 inset;
+            box-shadow: 0px 0px 20px #333 inset;
+          }
+  .big { font-size: 18px; font-weight: bold; padding: 5px 20px 5px 0px; }
+  .align-right { float: right; }
+  .align-center { text-align: center; font-size: 11px; }
+  .subhead { color: #40a2d5; }
+  .contents { font-size: 14px; }
+  .slogan { float: right; font-style: italic; color: #e9e9e9; font-size: 17px; padding-top: 15px; }
+  .black { color: #000; }
+  .clear { clear: both; }
+  ::selection { background: #F85510; /* Safari */ }
+  ::-moz-selection { background: #F85510; /* Firefox */ }
+</style>
 <p class="align-center">Jeg ønsker at se denne e-mail i min browser - <?php echo HTML::anchor('deals/email_format/'.$deals->ID, 'klik her'); ?>.</p>	
 <div id="main">
 	<div id="main-title">
@@ -85,6 +79,3 @@
 <p class="align-center">Du modtager denne e-mail fordi du er tilmeldt nyhedsbrevet hos TilbudiByen.dk.Hvis du ikke ønsker at modtage Dagens Tilbud i Byen på e-mail længere kan du altid afmelde dig ved at klike her.</p>
 
 <p class="align-center">Nyhedsbrevet udsendes af Tilbudibyen.dk ApS - Nørregade 7B - 1165 København K</p>
-
-</body>
-</html>
