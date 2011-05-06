f6562933d0dd7ca81102bd22cbf3bc4ca4501873
Paul Villacorta <pwvillacorta@gmail.com>
2011-05-06 09:11:10 +0800

    gmail html format

--- kohana/application/views/tilbud/template_email.php
+++ kohana/application/views/tilbud/template_email.php
@@ -42,29 +42,37 @@
 		<img src="<?php echo url::base(TRUE)?>images/logo.png" /><span style="float: right; font-style: italic; color: #e9e9e9; font-size: 17px; padding-top: 15px;">De bedste tilbud og oplevelser i din by!</span>
 	</div>
 	
-	<div id="deal-title">
-		<h1><span class="black"><?php echo $deals->title; ?></span>  <?php echo html_entity_decode($deals->description); ?></h1>
+	<div style="background: #40a2d5; color: #FFF;">
+		<h1><span style="color: #000;"><?php echo $deals->title; ?></span>  <?php echo html_entity_decode($deals->description); ?></h1>
 	</div>
 	
-	<div id="deal-control">
+	<div id="width: 200px; float: left;">
 		<?php $price = ($deals->regular_price * (100 - $deals->discount)) / 100; 
-					$class = strlen($price) > 5 ? 'medium' : ''; 
+					$class = strlen($price) > 5 ? ' font-size: 45px;' : ''; 
 					$savings = $deals->regular_price - $price; ?>
     
-		<div class="price <?php echo $class; ?>"><span class="black"><?php echo $price; ?></span>,-</div>
+		<div style="font-size: 60px; font-weight: bold; text-align: center; color: #40a2d5; <?php echo $class; ?>"><span style="color: #000;"><?php echo $price; ?></span>,-</div>
 		
-		<div><a href="<?php echo Url::base(TRUE) . 'deals/view/' . $deals->ID; ?>" class="button">Bestil nu!</a></div>
+		<div><a href="<?php echo Url::base(TRUE) . 'deals/view/' . $deals->ID; ?>" style="background: url(<?php echo url::base(TRUE)?>images/bg-button.png) top left repeat-x; 
+            color: #FFFFFF; font-weight: bold; font-size: 28px; 
+            padding: 10px 15px; text-align: center; margin-bottom: 20px;
+            border-radius: 10px; display: block; text-decoration: none; 
+            -moz-border-radius: 10px;
+            -webkit-border-radius: 10px;
+            -moz-box-shadow: 0px 0px 20px #333 inset;
+            -webkit-box-shadow: 0px 0px 20px #333 inset;
+            box-shadow: 0px 0px 20px #333 inset;">Bestil nu!</a></div>
 		
-		<div class="big">Værdi:		   <span class="align-right"><?php echo $deals->regular_price; ?>,-</span></div>
-		<div class="big">Rabat:		    <span class="align-right"><?php echo $deals->discount; ?> %</span></div>
-		<div class="big">Du sparer:	   <span class="align-right"><?php echo $savings; ?>,-</span></div>
-		<div class="clear"></div>
+		<div style="font-size: 18px; font-weight: bold; padding: 5px 20px 5px 0px;">Værdi:		   <span style="float: right;"><?php echo $deals->regular_price; ?>,-</span></div>
+		<div style="font-size: 18px; font-weight: bold; padding: 5px 20px 5px 0px;">Rabat:		    <span style="float: right;"><?php echo $deals->discount; ?> %</span></div>
+		<div style="font-size: 18px; font-weight: bold; padding: 5px 20px 5px 0px;">Du sparer:	   <span style="float: right;"><?php echo $savings; ?>,-</span></div>
+		<div style="clear: both;"></div>
 	
-		<h3 class="subhead">Information</h3>
-		<div class="contents"><?php echo $deals->information; ?></div>
+		<h3 style="color: #40a2d5;">Information</h3>
+		<div style="font-size: 14px;"><?php echo $deals->information; ?></div>
 
-		<h3 class="subhead">Hvor ligger det</h3>
-		<div class="contents"></div>
+		<h3 style="color: #40a2d5;">Hvor ligger det</h3>
+		<div style="font-size: 14px;"></div>
 		
 	</div>
 	
@@ -73,11 +81,11 @@
 		<img src="<?php echo url::base(TRUE)?>images/sample-image.jpg" width="445" height="300" />
 		<?php echo html_entity_decode($deals->contents); ?>
 	</div>
-	<div class="clear"></div>
+	<div style="clear: both;"></div>
   
   <div style="text-align: right; margin-top: 25px;"><?php echo HTML::Image(Url::base(TRUE).'images/facebook-like.png'); ?></div>
 </div>
 
-<p class="align-center">Du modtager denne e-mail fordi du er tilmeldt nyhedsbrevet hos TilbudiByen.dk.Hvis du ikke ønsker at modtage Dagens Tilbud i Byen på e-mail længere kan du altid afmelde dig ved at klike her.</p>
+<p style="text-align: center; font-size: 11px;">Du modtager denne e-mail fordi du er tilmeldt nyhedsbrevet hos TilbudiByen.dk.Hvis du ikke ønsker at modtage Dagens Tilbud i Byen på e-mail længere kan du altid afmelde dig ved at klike her.</p>
 
-<p class="align-center">Nyhedsbrevet udsendes af Tilbudibyen.dk ApS - Nørregade 7B - 1165 København K</p>
+<p style="text-align: center; font-size: 11px;">Nyhedsbrevet udsendes af Tilbudibyen.dk ApS - Nørregade 7B - 1165 København K</p>
