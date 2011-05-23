9d123fe0af714f5510ea608b28fbfea57e2a0020
Paul Villacorta <pwvillacorta@gmail.com>
2011-05-24 01:18:32 +0800

    added LBL_INVALID_EMAIL

--- kohana/application/languages/dk.php
+++ kohana/application/languages/dk.php
@@ -238,6 +238,7 @@ define("LBL_INVALID_SESSION", "Ugyldig session");
 define("LBL_SEND", "Send");
 define("LBL_SEND_ALL", "Send to Subscribers");
 define("LBL_NO_SUBSCRIBERS", "There are no subscribers on this city");
+define("LBL_INVALID_EMAIL", "Invalid Email Address");
 
 define("LBL_COMMA_DELIMITED", "Adskild med komma (,)");
 define("LBL_CONFIRM", "bekræft");
