228f3341a639b3e2dfd4c9284d576f5c5a22e770
Paul Villacorta <pwvillacorta@gmail.com>
2011-05-23 22:59:03 +0800

    fixed tinymce url rendering again

--- kohana/application/views/tilbud/admin/emails/view.php
+++ kohana/application/views/tilbud/admin/emails/view.php
@@ -23,23 +23,21 @@
 			<script type="text/javascript">
 				tinyMCE.init({
 					mode : "exact",
+					elements : 'abshosturls',
 					theme : "advanced",
 					width: "100%",
 					height: "600",
-					elements : 'relurlstopage',
-					relative_urls : true,
-					document_base_url : 'http://tilbudibyen.com/',
-					
-					plugins : "table,fullscreen,inlinepopups,advlink",
+					plugins : 'advlink,advimage,table,fullscreen,inlinepopups',
+					relative_urls : false,
+					remove_script_host : false,
 					
 					// Theme options
-        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect,|,bullist,numlist,|,outdent,indent,|,link,unlink,|,code,fullscreen",
-        theme_advanced_buttons2 : "",
-        theme_advanced_buttons3 : "",
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_toolbar_align : "center",
-        theme_advanced_statusbar_location : "bottom",
-
+					theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontsizeselect,|,bullist,numlist,|,outdent,indent,|,link,unlink,|,code,fullscreen",
+					theme_advanced_buttons2 : "",
+					theme_advanced_buttons3 : "",
+					theme_advanced_toolbar_location : "top",
+					theme_advanced_toolbar_align : "center",
+					theme_advanced_statusbar_location : "bottom",
 				});
 			</script>		
       
@@ -52,7 +50,7 @@
 						<?php echo Form::input('subject', $subject, array('style' => 'width: 99%')); ?>
         </li>
         <li><?php echo Form::label('body', __(LBL_EMAIL_BODY)); ?>
-						<?php echo Form::textarea('body', $body, array('id' => 'relurlstopage')); ?>
+						<?php echo Form::textarea('body', $body, array('id' => 'abshosturls')); ?>
         </li>
         <li>
             <?php echo Form::submit('submit', __(LBL_SEND)); ?>
