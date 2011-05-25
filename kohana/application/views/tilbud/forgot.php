03a6a8f685f14ddcd0ecf73146d7d69a190342ab
Mike <michaxze@gmail.com>
2011-05-24 20:34:22 -0500

    fixed layout

--- kohana/application/views/tilbud/forgot.php
+++ kohana/application/views/tilbud/forgot.php
@@ -1,50 +1,50 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
 <?php require_once 'header.php'; ?>
   <!-- content starts here -->
-  <section id="main-body">
+  <section id="ad-body">
   	<div class="centered">
 
        <script type="text/javascript">
           jQuery(document).ready(function() {
-        
             $("#forget_password").bind("submit", function() {
-              
             	if ($("#email").val().length < 1 || $("#email").val().length < 1) {
             	    $("#email_send_error").show().fadeOut(2000);
             	    return false;
             	}
-
             	$.fancybox.showActivity();
-
             	$.ajax({
             		type		: "POST",
             		cache	: false,
-            		url		: "/home/forgot",
+            		url		: "<?php echo Url::base(TRUE); ?>home/forgot",
             		data		: $(this).serializeArray(),
             		success: function(data) {
             			$.fancybox(data);
             		}
             	});
-
             	return false;
-            });
-            
+            });    
           });
         </script>
+      <div id="htitle">
+        <h2><?php echo __(LBL_CHANGE_YOUR_TILBUD_PASSWORD)?></h2>
+      </div>
 
-   <br/>
-   <h3><?php echo __(LBL_CHANGE_YOUR_TILBUD_PASSWORD)?></h3>
-   <p> <?php echo __(LBL_TILBUD_SEND_RESET_INSTRUCTIONS)?></p>
-   <br/>   
-   <form id="forget_password" action="" method="post">
-      <?php echo __(LBL_PLEASE_ENTER_EMAIL_ADDRESS); ?> <br/>
-    <div id="email_send_error" style="color: red; display: none"><?php echo __(LBL_PLEASE_ENTER_YOUR_EMAIL_ADDRESS); ?> </div>
-    <input type="text" name="email" id="email" value="" style="width:240px">
-    <input type="submit" name="submit" value="<?php echo __(LBL_SEND_INSTRUCTIONS);?>">
-    </form>
+			<div id="myforms">
+      <p> <?php echo __(LBL_TILBUD_SEND_RESET_INSTRUCTIONS)?></p>
 
-    </div>
-  </section>
+      <form id="forget_password" action="" method="post">
+      <ul>
+        <li><?php echo Form::label('email', __(LBL_PLEASE_ENTER_EMAIL_ADDRESS)); ?>
+            <?php echo isset($errors['cardnumber']) ? '<span class="serror">' . $errors['cardnumber'] . '</span>' : ''; ?>
+            <div id="email_send_error" style="color: red; display: none"><?php echo __(LBL_PLEASE_ENTER_YOUR_EMAIL_ADDRESS); ?></div>
+            <?php echo Form::input('email', '', array('required' => true, 'style' => 'width: 240px;', 'id' => 'email')); ?>
+            <?php echo Form::submit('submit', __(LBL_SEND_INSTRUCTIONS)); ?>
+        </li>
+      </ul>
+      <?php echo Form::close(); ?>
+      </div>
+   	</div>
+ 	</section>
   
   <!-- footer starts here -->
   <?php require_once 'footer.php'; ?>
