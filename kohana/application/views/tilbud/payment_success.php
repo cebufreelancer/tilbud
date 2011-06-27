8b2d2f994f70141b4b661f9e943c016a1df27b79
Mike <michaxze@gmail.com>
2011-06-27 17:34:53 +0800

    add new file

---
+++ kohana/application/views/tilbud/payment_success.php
@@ -0,0 +1,22 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.'); ?>
+<?php require_once 'header.php'; ?>
+<!-- content starts here -->
+<section id="ad-body">
+	<div class="centered">
+      <br/><br/>
+      
+
+      <br/><br/>
+      <h3>
+        <?php echo __(LBL_THANK_YOU_PAYMENT_SUCCESS_MESSAGE); ?>
+      </h3>
+
+
+ 	</div>
+</section>
+
+  <!-- footer starts here -->
+  <?php require_once 'footer.php'; ?>
+  
+</body>
+</html>
